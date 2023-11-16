80688844b72fe36f46e9af9f36466e87276f25a1
Matthew Frederes <matt@bitech.biz>
2023-11-15 21:43:42 -0800

    initial commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlexLayoutTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
